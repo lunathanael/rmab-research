98e29d25c4a90de0410be57889a33034c44d4c75
Nathanael Lu <lunathanael@gmail.com>
2025-07-29 21:49:30 -0400

    add build stuff

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+template<int H, int N, double alpha>
 class RMAB {
-    
+
 public:
-}
+};
 
 int main() {
-
+    RMAB<3, 10, 0.5> rmab;
     return 0;
 }
