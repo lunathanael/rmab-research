633ec15be3b2abcf694c7f2dcfdc0d9978060ff9
Nathanael Lu <lunathanael@gmail.com>
2025-07-30 00:10:24 -0400

    tmp

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,7 @@
 #include <array>
 #include <algorithm>
 #include <iostream>
+#include <vector>
 
 consteval int combinations(int n, int r) {
     if (r > n || r < 0 || n < 0) return 0;
@@ -15,7 +16,7 @@ consteval int combinations(int n, int r) {
     return static_cast<int>(result);
 }
 
-template<int N, int S>
+template<size_t N, size_t S>
 consteval void fill_states_helper(auto& states, int& idx, std::array<int, S>& current, int pos, int remaining) {
     if (pos == S - 1) {
         current[pos] = remaining;
@@ -29,7 +30,7 @@ consteval void fill_states_helper(auto& states, int& idx, std::array<int, S>& cu
     }
 }
 
-template<int N, int S>
+template<size_t N, size_t S>
 consteval auto generate_states() {
     constexpr int n = combinations(N + S - 1, S - 1);
     using state_t = std::array<int, S>;
@@ -43,6 +44,46 @@ consteval auto generate_states() {
     
     return states;
 }
+
+template<size_t S, int alpha_n, size_t actions_size = 0>
+consteval auto find_actions(const std::array<int, S> & state) {
+    std::vector<std::pair<int, std::vector<int>>> actions = {{0, {}}}, tmp;
+    // for (int i = 0; i < alpha_n; ++i) {
+    //     tmp.clear();
+    //     for (int j = 0; j < std::min(state[i], alpha_n) + 1; ++j) {
+    //         for (const auto & action : actions) {
+    //             if (action.first + j <= alpha_n) {
+    //                 auto new_action = action.second;
+    //                 new_action.push_back(j);
+    //                 tmp.push_back({action.first + j, new_action});
+    //             }
+    //         }
+    //         actions = std::move(tmp);
+    //     }
+    // }
+    if constexpr (actions_size == 0) {
+        return actions.size();
+    } else {
+        std::array<std::array<int, S>, actions_size> res;
+        for (int i = 0; i < actions_size; ++i) {
+            for(int j = 0; j < S; ++j) {
+                res[i][j] = actions[i].second[j];
+            }
+        }
+        return res;
+    }
+}
+
+template<int alpha_n, size_t S>
+consteval auto generate_actions(const std::array<int, S> & state) {
+    constexpr size_t actions_size = find_actions<S, alpha_n>(state);
+    return find_actions<S, alpha_n, actions_size>(state);
+}
+
+// template<size_t S, size_t n>
+// consteval auto generate_actions(const std::array<std::array<int, S>, n> & states) {
+    
+// }
 
 template<typename T, size_t S, size_t n>
 void print_states(const std::array<std::array<T, S>, n>& states) {
--- main.cpp
+++ main.cpp
@@ -8,13 +8,17 @@ template <
     array<array<array<double, 2>, S>, H> rewards,
     array<array<array<array<double, 2>, S>, S>, H> transition_probabilities>
 class RMAB {
-    
+    static constexpr const int alpha_n = alpha * N;
+    static_assert(alpha_n / alpha == N, "alpha * N must be an integer");
+    static constexpr const auto states = generate_states<N, S>();
 public:
 };
 
 
 int main() {
-    constexpr auto states = generate_states<10, 3>();
-    print_states(states);
+    constexpr auto states = generate_states<50, 4>();
+    // print_states(states);
+    constexpr auto actions = generate_actions<10>(states[0]);
+    // print_actions(actions);
     return 0;
 }
