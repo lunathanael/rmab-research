86935016f3051b57929469e11a29dcef58c1908a
Nathanael Lu <lunathanael@gmail.com>
2025-09-13 01:44:38 -0400

    profing time

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -7,7 +7,7 @@ int main() {
   RMAB rmab;
   rmab.parse();
   rmab.print_args();
-  for(int i : {10, 20, 30, 40, 50, 60, 70, 80})
+  for(int i : {10})
     cout << format("#Arms: {}, Opt: {}\n", i, rmab.solve(i));
   return 0;
 }
