068c14afb21a1e12583da01b9211680f878c5b42
Nathanael Lu <lunathanael@gmail.com>
2025-09-12 22:32:00 -0400

    action it

--- cpp/src/solve.cpp
+++ cpp/src/solve.cpp
@@ -6,11 +6,15 @@ using namespace std;
 
 void RMAB::solve() {
     DPLayer prev(n_arms, n_states), curr(n_arms, n_states);
-    DPStateIterator it(n_arms, n_states);
+    DPStateIterator sit(n_arms, n_states);
     for(int t = n_steps - 1; t >= 0; --t) {
-        it.init();
-        for(int i = 0; i < it.size(); i = it.next()) {
-
+        sit.init();
+        for(int i = 0; i < sit.size(); i = sit.next()) {
+            StateActionIterator ait(sit, n_alpha);
+            do {
+                
+            }
+            while(ait.next());
         }
     }
 }
